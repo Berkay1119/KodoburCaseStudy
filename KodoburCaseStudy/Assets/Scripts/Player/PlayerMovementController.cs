862987d1c65faaeacba41b306b648c4b7917c6d3
karakayaberkay <berkay11karakaya@gmail.com>
2023-10-09 21:10:54 +0300

    jump and DOTween

--- KodoburCaseStudy/Assets/Scripts/Player/PlayerMovementController.cs
+++ KodoburCaseStudy/Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,3 +1,5 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -5,10 +7,16 @@ public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] private float playerWalkSpeed;
     [SerializeField] private float playerRunSpeed;
+    [SerializeField] private float jumpHeight;
     private Vector3 _moveVector;
+    private bool _onJump;
     private void Update()
     {
         _moveVector= new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
     }
 
     private void FixedUpdate()
@@ -16,8 +24,26 @@ public class PlayerMovementController : MonoBehaviour
         Move(_moveVector, Input.GetKey(KeyCode.LeftShift) ? playerRunSpeed : playerWalkSpeed);
     }
 
+    private void Jump()
+    {
+        if (_onJump)
+        {
+            return;
+        }
+        _onJump = true;
+        float currentHeight = transform.position.y;
+        transform.DOMoveY(currentHeight+jumpHeight,playerRunSpeed).SetSpeedBased().SetLoops(2,LoopType.Yoyo).
+            OnComplete(()=>_onJump=false);
+
+        
+    }
+
     private void Move(Vector3 moveVector,float speed)
     {
+        if (_onJump)
+        {
+            return;
+        }
         Quaternion rotation = transform.rotation;
         if (moveVector.z!=0)
         {
